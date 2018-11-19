536edb7d41837586fe267f5cd3d72c83987e02b3
yugantar jain <yugantarjain@YuJs-MacBook-Pro.local>
2018-11-19 12:53:27 +0530

    Furniture AR

---
+++ Assets/Vuforia/Scripts/SceneChange.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneChange : MonoBehaviour {
+
+	public void change(string name)
+	{
+	    SceneManager.LoadScene(name);
+	}
+
+}
